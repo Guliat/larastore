02ac5399d9ef76297d25c9dfb2d612c14a147b5d
AleksandarEnchev <guliat88@mail.bg>
2020-09-13 00:43:09 +0300

    fix Laravel Mix, add devDependencies and run dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-]);
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.setPublicPath('public');
+mix.setResourceRoot('../');
